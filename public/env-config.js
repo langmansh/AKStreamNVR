70e3eb698bfbe482cca6d31505c1c8c946d5557e
langmansh <nobarking@vip.qq.com>
2021-11-28 01:56:20 +0800

    初始化

--- public/env-config.js
+++ public/env-config.js
@@ -1,4 +1,4 @@
 window._env_ = {
-  REACT_APP_API_HOST: "nvr.bsguaji.cn:10049",
-  AKSTREAM_WEB_API:"nvr.bsguaji.cn:10046",
+  REACT_APP_API_HOST: "11.11.201.22",
+  AKSTREAM_WEB_API:"11.11.201.22:5800",
 }
